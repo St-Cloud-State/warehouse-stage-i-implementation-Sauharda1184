aeaf5397140f564de96debebb26ca5ece5915023
Sauharda1184 <sauhardameher@gmail.com>
2024-12-07 13:59:54 -0600

    Warehouse Feat 1.0

---
+++ WareHouseStage1/Warehouse.java
@@ -0,0 +1,53 @@
+package WarehouseSystem;
+public class Warehouse {
+    // Private fields
+    Integer nextClientID;
+    Integer nextProductID;
+    ClientList clientList;
+    ProductList productList;
+    private static Warehouse warehouse;
+
+    // Constructor
+    private Warehouse(){ // Private to enforce singleton
+        clientList = ClientList.instance();
+        productList = ProductList.instance();
+        nextClientID = 0;
+        nextProductID = 0;
+    };
+
+    // Operations
+    public static Warehouse instance() { // Creation method
+        if (warehouse == null) {
+            return (warehouse = new Warehouse());
+        }
+        else {
+            return warehouse;
+        }
+    }
+
+    public Client addClient(String name, String email) {
+        Client client = new Client(Integer.toString(nextClientID), name, email);
+        nextClientID++;
+        if (clientList.addClient(client)) {
+            return client;
+        }
+        return null;
+    }
+
+    public Product addProduct(String name, Float price, Integer quantity) {
+        Product product = new Product(Integer.toString(nextProductID), name, price, quantity);
+        nextProductID++;
+        if (productList.addProduct(product)) {
+            return product;
+        }
+        return null;
+    }
+
+    public Client findClient(String clientID) {
+        return clientList.getClient(clientID);
+    }
+    public Product findProduct(String productID) {
+        return productList.getProduct(productID);
+    }
+
+}
